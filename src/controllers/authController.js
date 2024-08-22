05495c2897dd803d46e7322eed9d40bc1d6e6378
ibr <ibr.seckin@gmail.com>
2024-08-22 19:31:59 +0300

    up

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -79,7 +79,7 @@ module.exports.auth = {
 
     if (req?.file) {
       req.body.profilePic =
-        process.env.FRONT_HOST+"/api/pics/" + req.file.filename;
+        "https://chat-app-socket-fs.onrender.com/api/pics/" + req.file.filename;
     } else {
       req.body.gender === "male"
         ? (req.body.profilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`)
